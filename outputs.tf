d0c91aa6518e8305b31f61e186fe0464ff2076ff
Jonathan Shea <jon@samtek.io>
2026-05-19 12:30:43 -0700

    update to simpler OCI module contract

--- outputs.tf
+++ outputs.tf
@@ -1,32 +1,7 @@
-output "policy_ids" {
-  description = "Map of policy keys to their OCIDs"
-  value = merge(
-    { for k, v in oci_identity_policy.predefined_compartment : k => v.id },
-    { for k, v in oci_identity_policy.custom_compartment : k => v.id },
-    { for k, v in oci_identity_policy.predefined_tenancy : k => v.id },
-    { for k, v in oci_identity_policy.custom_tenancy : k => v.id }
-  )
+output "policy_id" {
+  value = oci_identity_policy.this.id
 }
 
-output "policy_names" {
-  description = "Map of policy keys to their names"
-  value = merge(
-    { for k, v in oci_identity_policy.predefined_compartment : k => v.name },
-    { for k, v in oci_identity_policy.custom_compartment : k => v.name },
-    { for k, v in oci_identity_policy.predefined_tenancy : k => v.name },
-    { for k, v in oci_identity_policy.custom_tenancy : k => v.name }
-  )
-}
-
-output "assigned_policies" {
-  description = "List of all policy statement templates that have been assigned"
-  value = distinct(concat(
-    var.predefined_policies,
-    flatten([for custom in var.custom_policies : custom.statements])
-  ))
-}
-
-output "assigned_principals" {
-  description = "List of all principals that have been assigned policies"
-  value       = var.group_principals
+output "policy_statements" {
+  value = local.rendered_policy_statements
 }
--- variables.tf
+++ variables.tf
@@ -1,33 +1,30 @@
-variable "predefined_policies" {
-  description = "List of OCI policy statement templates to assign to the group principals. Use {group} as placeholder for the group name."
-  type        = list(string)
-  default     = []
+variable "policy_name" {
+  type = string
 }
 
-variable "custom_policies" {
-  description = "List of custom OCI policy groups to create and assign"
-  type = list(object({
-    name        = string
-    description = string
-    statements  = list(string)
-  }))
-  default = []
+variable "policy_description" {
+  type    = string
+  default = "Managed by Helix Cloud Access Roles"
+}
+
+variable "policy_freeform_tags" {
+  description = "Freeform tags to apply to the OCI IAM policy."
+  type        = map(string)
+  default     = {}
+}
+
+variable "policy_compartment_id" {
+  type = string
+}
+
+variable "policy_scope" {
+  type = string
+}
+
+variable "policy_statement_templates" {
+  type = list(string)
 }
 
 variable "group_principals" {
-  description = "List of OCI group names to assign policies to"
-  type        = list(string)
-  default     = []
-}
-
-variable "compartment_id" {
-  description = "The OCI compartment OCID to assign policies at. Must provide either compartment_id or tenancy_id."
-  type        = string
-  default     = null
-}
-
-variable "tenancy_id" {
-  description = "The OCI tenancy OCID to assign policies at. Must provide either compartment_id or tenancy_id."
-  type        = string
-  default     = null
+  type = list(string)
 }
